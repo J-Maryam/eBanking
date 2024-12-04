4f86dac7d8fd183d1cef0e63d6804176dfe32f94
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-04 15:33:20 +0100

    feat(role): create role entity with attribus: id and name

---
+++ src/main/java/org/youcode/ebanking/models/Role.java
@@ -0,0 +1,24 @@
+package org.youcode.ebanking.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+}
