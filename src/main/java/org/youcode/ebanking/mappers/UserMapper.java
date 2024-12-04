b47dfc82a04d74e80283b866083878bebec8b2fa
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-04 11:47:56 +0100

    feat(mapper): created mapper (UserMapper) and added toEntity and ToDTO methods for mapping

--- src/main/java/org/youcode/ebanking/mappers/UserMapper.java
+++ src/main/java/org/youcode/ebanking/mappers/UserMapper.java
@@ -1,4 +1,11 @@
 package org.youcode.ebanking.mappers;
 
+import org.mapstruct.Mapper;
+import org.youcode.ebanking.dtos.UserRegistrationDto;
+import org.youcode.ebanking.models.User;
+
+@Mapper
 public interface UserMapper {
+    User toEntity(UserRegistrationDto dto);
+    UserRegistrationDto toDTO(User user);
 }
