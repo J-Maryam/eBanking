4eb898be14a31a052363630905111144c1eb8649
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-04 15:22:40 +0100

    feat(service): create UserService interface and service implementation,and added registerUser method to register as a new user

--- src/main/java/org/youcode/ebanking/services/UserService.java
+++ src/main/java/org/youcode/ebanking/services/UserService.java
@@ -1,4 +1,8 @@
 package org.youcode.ebanking.services;
 
+import org.youcode.ebanking.dtos.UserRegistrationDto;
+import org.youcode.ebanking.dtos.UserResponseDto;
+
 public interface UserService {
+    UserResponseDto registerUser(UserRegistrationDto dto);
 }
--- src/main/java/org/youcode/ebanking/services/UserServiceImpl.java
+++ src/main/java/org/youcode/ebanking/services/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package org.youcode.ebanking.services;
 
-public class UserServiceImpl {
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.youcode.ebanking.dtos.UserRegistrationDto;
+import org.youcode.ebanking.dtos.UserResponseDto;
+import org.youcode.ebanking.mappers.UserMapper;
+import org.youcode.ebanking.models.AppUser;
+import org.youcode.ebanking.repositories.UserRepository;
+
+@Service
+@Transactional
+@Validated
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto registerUser(UserRegistrationDto dto) {
+        if(userRepository.findByUsername(dto.username()).isPresent()){
+            throw new EntityExistsException("Username '"+dto.username()+"' already exists");
+        }
+        AppUser appUser = userMapper.toEntity(dto);
+        appUser.setRole("USER");
+        AppUser saved = userRepository.save(appUser);
+        return userMapper.toDTO(saved);
+    }
 }
